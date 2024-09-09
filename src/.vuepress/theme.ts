115e58c643ef3b5f0aa2a6604e78fe6f8c2f9508
lhccong <liuhuaicong@zhut.cn>
2024-09-09 15:41:36 +0800

    fix：更新github地址

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -6,7 +6,7 @@ export default hopeTheme({
   hostname: "https://vuepress-theme-hope-docs-demo.netlify.app",
 
   author: {
-    name: "小傅哥",
+    name: "聪",
     url: "https://mister-hope.com",
   },
 
@@ -14,7 +14,7 @@ export default hopeTheme({
 
   logo: "/logo.svg",
 
-  repo: "fuzhengwei/xfg-resume-blog",
+  repo: "lhccong",
 
   docsDir: "src",
 
@@ -24,7 +24,7 @@ export default hopeTheme({
   // sidebar
   sidebar,
 
-  footer: "我的微信：fustack",
+  footer: "我的微信：lhc_iuuaiong",
 
   displayFooter: true,
 
