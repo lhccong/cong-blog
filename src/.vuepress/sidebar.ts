3bbb1dc3f65c64be12d8c8600d4384a65086dfe1
lhccong <liuhuaicong@zhut.cn>
2024-09-10 08:59:58 +0800

    fix：更新文档

--- src/.vuepress/sidebar.ts
+++ src/.vuepress/sidebar.ts
@@ -1,13 +1,13 @@
 import { sidebar } from "vuepress-theme-hope";
 
 export default sidebar({
-  "/": [
-    "",
-    {
-      text: "SQL Dog 项目开发日志",
-      icon: "laptop-code",
-      prefix: "md/dev-log/",
-      children: ["day01.md","day02.md","day03.md","day04.md","day05.md","day06.md","day07~day08.md","day09~day10.md","day11.md","day12.md"],
-    },
-  ],
+  // "/": [
+  //   "",
+  //   {
+  //     text: "SQL Dog 项目开发日志",
+  //     icon: "laptop-code",
+  //     prefix: "md/dev-log/",
+  //     children: ["day01.md","day02.md","day03.md","day04.md","day05.md","day06.md","day07~day08.md","day09~day10.md","day11.md","day12.md"],
+  //   },
+  // ],
 });
